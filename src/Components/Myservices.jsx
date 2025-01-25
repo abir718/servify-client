76a0b3558c2e423f5e3db96214ffc7ce4f8e7afe
abir718 <abir.hasan2003s@gmail.com>
2025-01-25 19:06:57 +0600

    delete service added

--- src/Components/Myservices.jsx
+++ src/Components/Myservices.jsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { authContext } from "../Authprovider";
+import { FaTrash, FaPen } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const Myservices = () => {
     const services = useLoaderData();
@@ -14,7 +16,34 @@ const Myservices = () => {
         }
     }, [services, user]);
 
-
+    const removeService = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/services/${id}`, {
+                    method: "DELETE",
+                })
+                    .then((res) => res.json())
+                    .then((data) => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your service has been deleted.",
+                                icon: "success",
+                            });
+                            setservice(service.filter((ser) => ser._id !== id));
+                        }
+                    });
+            }
+        });
+    };
 
     return (
         <div className="bg-base-200">
@@ -28,6 +57,10 @@ const Myservices = () => {
                         <p className="text-gray-500 border-[2px] py-1 px-2 rounded-full w-fit">{service.category}</p>
                         <p className="w-80">{service.description}</p>
                         <p>Pricing: {service.price}$</p>
+                        <div className="flex gap-3">
+                            <button><FaPen size="32" className="text-white bg-[#2C485F] p-2 rounded-lg" /></button>
+                            <button><FaTrash onClick={() => removeService(service._id)} size="32" className="text-white bg-[#2C485F] p-2 rounded-lg" /></button>
+                        </div>
                     </div>
                 </div>)}
             </div>
